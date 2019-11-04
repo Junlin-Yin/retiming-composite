9a89529cc2eef6f0b1cabb4e18e8dafe2d3b0c5c
Junlin-Yin <yjl1217265@163.com>
2019-11-04 16:55:17 +0800

    retiming almost done

--- retiming.py
+++ retiming.py
@@ -5,7 +5,7 @@ import cv2
 import math
 import librosa
 import numpy as np
-from __init__ import detector, predictor, vfps
+from __init__ import detector, predictor, vfps, size
 from __init__ import ref_dir
 
 def test_aloud(mp3_path, thr1=1e-3, thr2=0.5, ksize=3, n=100):
@@ -88,11 +88,11 @@ def test_motion(tar_path, aB=1, blink_thr=0.025, ksize=3):
     V += aB * B
     return V
 
-def match_penalty(A, V, au=2):
+def match_penalty(A, V, aU=2):
     G = np.zeros((A.shape[0], V.shape[0]))
     for i in range(G.shape[0]):
         term1 = V if A[i] == 0 else np.zeros(V.shape)
-        term2 = au*V if i >= 3 and A[i-2] == 1 and A[i-3] == 0 else np.zeros(V.shape)
+        term2 = aU*V if i >= 3 and A[i-2] == 1 and A[i-3] == 0 else np.zeros(V.shape)
         G[i, :] = term1 - term2
     return G
 
@@ -102,7 +102,7 @@ def preprocess(mp3_path, tar_path, npz_path):
     G = match_penalty(A, V)
     np.savez(npz_path, A=A, V=V, G=G)
     
-def retiming(mp3_path, tar_path, tmp_path=None, preproc=False):
+def timing_opt(mp3_path, tar_path, tmp_path=None, preproc=False, aS=2):
     # check if preprocessing is needed
     _, fname = os.path.split(mp3_path)
     mp3_id, _ = os.path.splitext(fname)
@@ -111,14 +111,67 @@ def retiming(mp3_path, tar_path, tmp_path=None, preproc=False):
     tmp_path = '%s%s-x%s' % (ref_dir, mp3_id, tar_id) if tmp_path is None else tmp_path
     if preproc or os.path.exists(tmp_path) == False:
         preprocess(mp3_path, tar_path, tmp_path)
-        
-    data = np.load(tmp_path)
-    V, G = data['V'], data['G']
     
+    # initialize data
+    data = np.load(tmp_path)
+    A, V, G = data['A'], data['V'], data['G']
+    N, M = G.shape
+    F = np.array([np.inf]*N*M*2).reshape(N, M, 2)
+    F[0, :, 0] = V if A[0] == 0 else 0
+    
+    # dynamic programming
+    for i in range(1, N):
+        if i % 100 == 0 or i == N-1:
+            print('dynamic programming: %04d/%04d' % (i, N-1))
+        for j in range(1, M):
+            F[i, j, 0] = min(F[i-1, j-1, 0], F[i-1, j-1, 1]) + G[i, j]
+            F[i, j, 1] = F[i-1, j, 0] + aS*V[j] + G[i, j]
+    
+    # optimize and backtrack
+    _, js, ks = np.where(F == np.min(F[N-1, :, :]))
+    assert(len(js) == 1 and len(ks) == 1)
+    j, k = js[0], ks[0]
+    L = np.zeros((N,))
+    for i in range(N-1, -1, -1):
+        # from N-1 to 0
+        L[i] = j
+        if i > 0:
+            if k == 0:
+                k = 0 if F[i-1, j-1, 0] < F[i-1, j-1, 1] else 1
+                j -= 1
+            else:
+                k -= 1
+    return L
 
-if __name__ == '__main__':
+def warp(prevfr, nextfr):
+    pass
+    
+def retiming(tar_path, save_path, L):
+    cap = cv2.VideoCapture(tar_path)
+    writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'DIVX'), vfps, size)
+    idx = 0
+    print('Start preprocessing...')
+    while cap.isOpened():
+        if idx == L.shape[0]:
+            break
+        cap.set(cv2.CAP_PROP_POS_FRAMES, L[idx])
+        _, frame = cap.read()
+        writer.write(frame)
+        
+        idx += 1
+        if idx % 100 == 0 or idx == L.shape[0]:
+            print("%s: %04d/%04d" % (save_path, idx, L.shape[0]))
+    print('Done')
+    
+def run():
     mp3_path = 'input/test036.mp3'
     tar_path = 'target/target001.mp4'
-    save_path = 'reference/retiming_t36i1.npz'
-    preprocess(mp3_path, tar_path, save_path)
-    print('Hello, World')
+    tmp_path = 'reference/retiming_t36i1.npz'
+    save_path = 'target/rt_target001.mp4'
+    L = timing_opt(mp3_path, tar_path, tmp_path)
+#    np.save('reference/L.npy', L)
+    print('Start from frame %04d.' % L[0])
+    retiming(tar_path, save_path, L)    
+
+if __name__ == '__main__':
+    run()
